753d69f42cae8bc8a71df35ccc6eb0661c49b6aa
Rahul Patle <rahulpatle101@gmail.com>
2016-01-01 21:03:19 -0700

    added cocoa pods, api request working, Finalized! :D

---
+++ Pokemon/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Pokemon
+//
+//  Created by Rahul Patle on 1/1/16.
+//  Copyright © 2016 CODECOOP. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+//simple closure which takes nothing and returns nothing
+typealias DownloadComplete = () -> ()
--- Pokemon/PokemonDetailVC.swift
+++ Pokemon/PokemonDetailVC.swift
@@ -11,12 +11,58 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     @IBOutlet var nameLbl: UILabel!
+    @IBOutlet var mainImg: UIImageView!
+    @IBOutlet var descriptionLbl: UILabel!
+    @IBOutlet var typeLbl: UILabel!
+    @IBOutlet var defenseLbl: UILabel!
+    @IBOutlet var heightLbl: UILabel!
+    @IBOutlet var pokedexLbl: UILabel!
+    @IBOutlet var weightLbl: UILabel!
+    @IBOutlet var attackLbl: UILabel!
+    @IBOutlet var evoLbl: UILabel!
+    @IBOutlet var currentEvoImg: UIImageView!
+    @IBOutlet var nextEvoImg: UIImageView!
+    
+    
     var pokemon: Pokemon!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLbl.text = pokemon.name
+        nameLbl.text = pokemon.name.capitalizedString
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        mainImg.image = img
+        
+        currentEvoImg.image = img
+        pokemon.downloadPokemonDetails { () -> () in
+            //this will be called after download is done
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        descriptionLbl.text = pokemon.description
+        typeLbl.text = pokemon.type
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        pokedexLbl.text = "\(pokemon.pokedexId)"
+        weightLbl.text = pokemon.weight
+        attackLbl.text = pokemon.attack
+        
+        if pokemon.nextEvolutionId == "" {
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.hidden = true
+        } else {
+            nextEvoImg.hidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            var str = "Next Evolution: \(pokemon.nextEvolutionTxt)"
+            
+            if pokemon.nextEvolutionLvl == "" {
+                str += " - LVL \(pokemon.nextEvolutionTxt)"
+            }
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,14 +71,9 @@ class PokemonDetailVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
-
 }
